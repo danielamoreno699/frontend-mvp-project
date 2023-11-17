a8196bd7c4e975cb57a6ddb60a3560f996339a67
Daniela Moreno <dani9292@hotmail.com>
2023-11-17 10:50:21 -0500

    set store from redux for initial config

--- aceclash-project/src/main.jsx
+++ aceclash-project/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { AceclashApp } from './aceclashApp'
+import { store } from './store'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <AceclashApp/>
       </BrowserRouter>
