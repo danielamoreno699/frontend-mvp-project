38c77c3d5c40414064a8c097513b148a1f9aca02
Daniela Moreno <dani9292@hotmail.com>
2023-11-17 13:59:05 -0500

    add css for registration  component

--- aceclash-project/src/auth/components/Register.jsx
+++ aceclash-project/src/auth/components/Register.jsx
@@ -1,7 +1,71 @@
-
+import '../styles/register.css'
 
 export const RegisterNewUser = () => {
   return (
-    <h1>Register</h1>
+    <>
+    <div className="center-container">
+    <section className='registration-section'> 
+        
+
+        <h1>Register</h1>
+        <form >
+          
+            <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            placeholder="Username"
+            />
+
+            <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            required
+            placeholder="Lastname"
+            />
+
+            <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Email"
+            />
+
+            <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            placeholder="Password"
+            />
+
+            <input
+            type="password"
+            id="ConfirmPassword"
+            name="ConfirmPassword"
+            required
+            placeholder="Confirm Password"
+            />
+          <button >
+                Sign up
+          </button>
+
+                <p>
+                    Already registered? <br/>
+                    <span className="line">
+                        <a href="/login">Login</a>
+                    </span>
+
+                </p>
+
+        </form>
+
+    </section>
+    </div>
+    
+    </>
   )
 }
