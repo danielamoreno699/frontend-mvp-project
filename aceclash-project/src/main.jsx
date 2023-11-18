fcd0c94f0665c9d0a1ae4dc9b45cae11a2f1815a
Daniela Moreno <dani9292@hotmail.com>
2023-11-18 17:47:54 -0500

    add provider

--- aceclash-project/src/main.jsx
+++ aceclash-project/src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter } from 'react-router-dom'
 import { AceclashApp } from './aceclashApp'
+import { Provider } from 'react-redux'
+import { store } from '../src/store/store'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   
+    <Provider store={store}>
       <BrowserRouter>
         <AceclashApp/>
       </BrowserRouter>
-
+    </Provider>
    
    
   </React.StrictMode>,
--- aceclash-project/src/router/AppRouter.jsx
+++ aceclash-project/src/router/AppRouter.jsx
@@ -1,16 +1,23 @@
 // AppRouter.js
 
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate,  Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { useSelector } from 'react-redux';
+
 
 export const AppRouter = () => {
+    const authStatus = useSelector((state) => state.auth.status);
   return (
     <Routes>
     
       <Route path="/auth/*" element={<AuthRoutes />} />
 
-      
-      <Route path="/*" element={<Navigate to="/auth/login" />} />
+        {authStatus === 'authenticated' ? (
+          <h1>hello, im user  </h1>
+        ) : (    
+          <Navigate to="/auth/login" />
+        )}
+    
     </Routes>
   );
 };
