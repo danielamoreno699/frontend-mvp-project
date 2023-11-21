78d14a43aa9c8e0b16f66673245d5c4f985db83c
Daniela Moreno <dani9292@hotmail.com>
2023-11-21 16:18:02 -0500

    fix useEffect function for data persistance

--- aceclash-project/src/router/AppRouter.jsx
+++ aceclash-project/src/router/AppRouter.jsx
@@ -9,14 +9,25 @@ import { persistLogin } from "../store/auth";
 export const AppRouter = () => {
     const authStatus = useSelector((state) => state.auth.status);
     const user = useSelector((state) => state.auth.user);
+    
+    
     const dispatch = useDispatch();
+    //const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const storedUser = JSON.parse(localStorage.getItem('user'));
-        if (storedUser) {
-          dispatch(persistLogin(storedUser._id));
-        }
-      }, []);
+        const fetchPersistedUser = async () => {
+            const storedUser = JSON.parse(localStorage.getItem('user'));
+            if (storedUser) {
+                await dispatch(persistLogin(storedUser._id));
+            }
+        };
+
+        fetchPersistedUser();
+    }, [dispatch]);
+
+    if (authStatus === 'checking') {
+        return <div>Loading...</div>;
+    }
 
   
     return (
