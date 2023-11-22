c3c19ce78e1681dd04200ecb393e203f8ac890e5
Daniela Moreno <dani9292@hotmail.com>
2023-11-21 20:30:39 -0500

    add some style to sidenavbar

---
+++ aceclash-project/src/admin/routes/AdminRoutes.jsx
@@ -0,0 +1,13 @@
+import {  Routes } from 'react-router-dom'
+import NavbarAdmin from '../components/Navbar'
+
+export const AdminRoutes = () => {
+  return (
+    <>
+    <NavbarAdmin/>
+    <Routes>
+   
+    </Routes>
+    </>
+  )
+}
