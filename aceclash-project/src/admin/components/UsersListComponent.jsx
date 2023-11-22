e71cb44c6adff00b2fad0587e1762549e92a5070
Daniela Moreno <dani9292@hotmail.com>
2023-11-22 16:22:36 -0500

    change for table

--- aceclash-project/src/admin/components/UsersListComponent.jsx
+++ aceclash-project/src/admin/components/UsersListComponent.jsx
@@ -1,67 +1,72 @@
-import '../styles/userList.css';
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
 import { getAllUsers } from '../../store/user';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { useState } from 'react';
 import UserUpdateComponent from './UserUpdateComponent';
-
 
 const UsersListComponent = () => {
   const dispatch = useDispatch();
-  const { users} = useSelector((state) => state.users);
+  const { users } = useSelector((state) => state.users);
   const [showModal, setShowModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
-
 
   useEffect(() => {
     dispatch(getAllUsers());
   }, [dispatch]);
 
-
-    const onHandleUpdateUser = (UserId) => {
-
-        setSelectedUser(UserId)
-        setShowModal(true);
-        console.log('update user', UserId);
-    }
-
+  const onHandleUpdateUser = (userId) => {
+    setSelectedUser(userId);
+    setShowModal(true);
+    console.log('update user', userId);
+  };
 
   return (
     <>
       <section className="container-section">
         <h1>Users List</h1>
-        <div className='users-container'>
-          {users.map((user) => (
-            <Card key={user._id} style={{ width: '20rem', margin: '2rem' }}>
-              <Card.Img variant="top" src={user.img} alt='img' />
-              <Card.Body>
-                <Card.Title>{user.name}</Card.Title>
-                <Card.Text>
-                  <p>{user.last_name}</p>
-                  <p>{user.role}</p>
-                  <p>{user.email}</p>
-                </Card.Text>
-                <div className='d-flex justify-content-around'>
-                <Button variant="primary" className="mr-3" onClick={() => onHandleUpdateUser(user._id)}>Update User</Button>
-                <Button variant="primary">See User Details</Button>
-              </div>
-              </Card.Body>
-            </Card>
-          ))}
+        <div className="users-container">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th>Email</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user._id}</td>
+                  <td>{user.name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.role}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <div className="d-flex justify-content-around">
+                      <Button
+                        variant="primary"
+                        className="mr-3"
+                        onClick={() => onHandleUpdateUser(user._id)}
+                      >
+                        Update User
+                      </Button>
+                      <Button variant="primary">See User Details</Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </div>
       </section>
 
-      <UserUpdateComponent
-        show={showModal}
-        setShow={setShowModal}
-        user={selectedUser}
-      
-      />
+      <UserUpdateComponent show={showModal} setShow={setShowModal} user={selectedUser} />
     </>
   );
+};
 
-
-  }
 export default UsersListComponent;
