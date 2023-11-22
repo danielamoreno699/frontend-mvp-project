43633e8be285ee84602803856434a5089817eed4
Daniela Moreno <dani9292@hotmail.com>
2023-11-22 13:17:15 -0500

    set card for user

--- aceclash-project/src/admin/components/UsersListComponent.jsx
+++ aceclash-project/src/admin/components/UsersListComponent.jsx
@@ -1,19 +1,33 @@
 
+import '../styles/userList.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
-const UsersListComponent = () => {
- 
+ const UsersListComponent = () => {
+  return (
     <>
-
+   
     <section className="container-section">
-          <h2>list of users</h2>
-          <div className="wrapper">
+      <h1>Users List</h1>
+      <div className='users-container'>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the cards content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        
+      </div>
 
-            
-          </div>
     </section>
     </>
-  
+  )
 }
 
 
--- aceclash-project/src/admin/routes/AdminRoutes.jsx
+++ aceclash-project/src/admin/routes/AdminRoutes.jsx
@@ -1,15 +1,18 @@
+// Import necessary dependencies
 
 import { Routes, Route } from 'react-router-dom';
 import NavbarAdmin from '../components/Navbar';
 import UsersListComponent from '../components/UsersListComponent';
 import '../styles/indexAdmin.css';
 
+// Define the AdminRoutes component
 export const AdminRoutes = () => {
   return (
     <div className="admin-container">
       <NavbarAdmin />
       <div className="admin-content">
         <Routes>
+        
           <Route path="/" element={<UsersListComponent />} />
         </Routes>
       </div>
