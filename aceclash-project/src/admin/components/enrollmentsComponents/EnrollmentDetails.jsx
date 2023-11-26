2f6a8c011d22e1a592a65691ade960857464c806
Daniela Moreno <dani9292@hotmail.com>
2023-11-25 19:39:04 -0500

    fix issue with displaying enrollments

--- aceclash-project/src/admin/components/enrollmentsComponents/EnrollmentDetails.jsx
+++ aceclash-project/src/admin/components/enrollmentsComponents/EnrollmentDetails.jsx
@@ -2,10 +2,14 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getEnrollmentById } from '../../../store/enrollments';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
 const EnrollmentDetails = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
+const navigate = useNavigate();
 
   const { enrollmentInf } = useSelector((state) => state.enrollments);
 
@@ -23,12 +27,59 @@ const EnrollmentDetails = () => {
     return <div>No enrollment data available</div>;
   }
 
+    const handleGoBack = () => {
+    navigate(-1);
+    }
+
   return (
     <div>
-      <h1>{enrollment.league}</h1>
-      {/* Access other properties here */}
-    </div>
-  );
+        <h1>Enrollment Details</h1>
+        <Table striped bordered hover>
+          <tbody>
+            <tr>
+              <td>User Name</td>
+              <td>{enrollment.userId.name}</td>
+            </tr>
+            <tr>
+              <td>User Last Name</td>
+              <td>{enrollment.userId.last_name }</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{enrollment.userId.email}</td>
+            </tr>
+            <tr>
+              <td>Name Tournament</td>
+              <td>{enrollment.tournamentId.name }</td>
+            </tr>
+            <tr>
+              <td>League</td>
+              <td>{enrollment.league}</td>
+            </tr>
+            <tr>
+              <td>Club</td>
+              <td>{enrollment.club}</td>
+            </tr>
+            <tr>
+              <td>Category</td>
+              <td>{enrollment.category}</td>
+            </tr>
+            <tr>
+              <td>Practice Location</td>
+              <td>{enrollment.practice_location}</td>
+            </tr>
+          </tbody>
+        </Table>
+        <div className="d-flex justify-content-end">
+          <Button variant="primary" onClick={handleGoBack}>
+            Go Back
+          </Button>
+          <Button variant="secondary" className="ml-2">
+            Update Tournament
+          </Button>
+        </div>
+      </div>
+  )
 };
 
 export default EnrollmentDetails;
