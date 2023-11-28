6d1100015daa0714fed22fd03c5721c688e0503a
Daniela Moreno <dani9292@hotmail.com>
2023-11-28 16:35:41 -0500

    implement logout

--- aceclash-project/src/users/components/NavbarUser.jsx
+++ aceclash-project/src/users/components/NavbarUser.jsx
@@ -1,15 +1,27 @@
 import Navbar from 'react-bootstrap/Navbar';
-import { useSelector} from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import { AiFillFacebook, AiFillGoogleCircle, AiFillPhone,  AiOutlineTwitter, AiTwotoneMail } from 'react-icons/ai';
 import '../styles/Navbar.css'
+import { logout } from '../../store/auth';
+import { useNavigate } from 'react-router-dom';
+
 
  const NavbarUser = () => {
  
     const user = useSelector((state) => state.auth.user);
     const userDataString = JSON.parse(localStorage.getItem('user'));
     const userId = user ? userDataString._id : null;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const onLogout = () => {
+        dispatch(logout());
+        navigate('/auth/login');
+        
+    }
  
     return (
     <header className="nav-container">
@@ -25,7 +37,7 @@ import '../styles/Navbar.css'
             <ul className="nav flex-column m-3 d-flex">
 
                 <li>
-                    <a className="nav-link" href="#" >Logout</a>
+                    <a className="nav-link" href="#" onClick={onLogout}>Logout</a>
                 </li>
                 <li>
                     <a className="nav-link" href="/">display tournaments</a>
